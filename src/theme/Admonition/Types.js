870d9bbbcd64ed93a02b9993b4d74d9a2381d51c
nanangsb <sefida.nanang@gmail.com>
2024-06-15 19:09:46 +0700

    Update Types.js

--- src/theme/Admonition/Types.js
+++ src/theme/Admonition/Types.js
@@ -8,7 +8,7 @@ import AdmonitionTypeCaution from '@theme/Admonition/Type/Caution';
 
 function MyCustomAdmonition(props) {
   return (
-    <div style={{margin: '5px', backgroundColor: '#90EE90', border: 'solid green', padding: 10, borderRadius: '25px'}}>
+    <div style={{margin: '10px', backgroundColor: '#e5f6e6', border: 'solid green', padding: 10, borderRadius: '25px'}}>
       <h5 style={{fontSize: 30}}>{props.title}</h5>
       <div>{props.children}</div>
     </div>
