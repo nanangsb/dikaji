456c679a96afe52ec8acaadae49b68de4be08219
nanangsb <sefida.nanang@gmail.com>
2024-06-15 14:03:04 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,10 @@ const config = {
           // Remove this to remove the "edit this page" links.
           //editUrl:
           //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        admonitions: {
+            keywords: ['my-custom-admonition'],
+            extendDefaults: true,
+          },
         },
         blog: {
           showReadingTime: true,
