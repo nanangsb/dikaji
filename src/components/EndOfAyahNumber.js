96fc695e1053051d1e85543346236372dec486e7
nanangsb <sefida.nanang@gmail.com>
2024-06-16 07:27:55 +0700

    Create EndOfAyahNumber.js

---
+++ src/components/EndOfAyahNumber.js
@@ -0,0 +1,14 @@
+// EndOfAyahNumber.js
+import React from 'react';
+import './EndOfAyahNumber.css';
+
+const EndOfAyahNumber = ({ number }) => {
+  return (
+    <div className="end-of-ayah-container">
+      {number}
+      <span className="end-of-ayah-symbol">۝</span>
+    </div>
+  );
+};
+
+export default EndOfAyahNumber;
