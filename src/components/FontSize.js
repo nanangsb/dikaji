7299b636651da05710d8a3c4c6207d438f8cb2b2
nanangsb <sefida.nanang@gmail.com>
2024-06-16 16:42:18 +0700

    Create FontSize.js

---
+++ src/components/FontSize.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FontSize({children, size}) {
+  return (
+    <span
+      style={{
+        fontSize: size,
+        padding: '0.2rem',
+      }}>
+      {children}
+    </span>
+  );
+}
