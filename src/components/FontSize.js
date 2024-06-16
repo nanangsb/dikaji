140629f9328f05016cdb1c30208feccc1a192eeb
nanangsb <sefida.nanang@gmail.com>
2024-06-16 17:09:39 +0700

    Update FontSize.js

--- src/components/FontSize.js
+++ src/components/FontSize.js
@@ -6,6 +6,7 @@ export default function FontSize({children, size}) {
       style={{
         fontSize: size,
         padding: '0.2rem',
+        direction: rtl;
       }}>
       {children}
     </span>
