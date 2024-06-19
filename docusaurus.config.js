cf99b54cef8723c8f9609a76cc7a352d51dab312
nanangsb <sefida.nanang@gmail.com>
2024-06-20 06:35:08 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,12 +113,7 @@ plugins: [
             position: 'left',
             label: 'Panduan',
           },
-          {
-            type: 'docSidebar',
-            sidebarId: 'quran',
-            position: 'left',
-            label: 'Quran',
-          },
+          {to: '/quran', label: 'Quran', position: 'left'},
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/nanangsb/dikaji',
