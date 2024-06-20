d28c9bd86df6ade10309411bb5ffb4443ff145df
nanangsb <sefida.nanang@gmail.com>
2024-06-20 13:06:34 +0700

    Update NewCard.js

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 export default function NewCard({children}) {
   return (
-    <>
     <div
       style={{
         fontSize: 'xx-large',
         padding: '0.2rem',
-        textAlign: 'right',
-        direction: 'rtl',
         width: '50vw',
         border: 'thin solid green',
       }}>
       {children}
     </div>
-  <hr />
-      </>
   );
 }
